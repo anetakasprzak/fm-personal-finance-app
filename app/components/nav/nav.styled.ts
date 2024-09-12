0e524fcba4c22261d5fbf3a838fddd2303fbf554
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-09-12 14:39:00 +0100

    tablet nav styles

--- app/components/nav/nav.styled.ts
+++ app/components/nav/nav.styled.ts
@@ -9,8 +9,30 @@ export const NavContainer = styled.ul`
   align-self: flex-end;
   padding: 0.8rem 1.6rem 0.8rem 1.6rem;
   border-radius: 0.8rem 0.8rem 0rem 0rem;
+
+  @media screen and (min-width: 29rem) {
+    padding: 0.8rem 2.8rem 0.8rem 2.8rem;
+  }
+
+  @media screen and (min-width: 35rem) {
+    padding: 1rem 4rem 0.8rem 4rem;
+  }
+`;
+
+export const NavEl = styled.li`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
 `;
 
 export const NavText = styled.p`
   display: none;
+  color: #b3b3b3;
+  font-size: 1.2rem;
+
+  @media screen and (min-width: 35rem) {
+    display: block;
+  }
 `;
--- app/components/nav/nav.tsx
+++ app/components/nav/nav.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
 import data from "../../data/data.json";
-import { NavContainer, NavText } from "./nav.styled";
+import { NavContainer, NavText, NavEl } from "./nav.styled";
 
 export default function Nav() {
   return (
     <NavContainer>
       {data.nav.map((nav) => (
-        <li key={nav.id}>
+        <NavEl key={nav.id}>
           <Image src={nav.image} alt="nav icon" width={24} height={24} />
           <NavText>{nav.text}</NavText>
-        </li>
+        </NavEl>
       ))}
     </NavContainer>
   );
